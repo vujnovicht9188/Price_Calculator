c9b2660376bb8575206e6bdf83a0025be15e99f6
vujnovicht9188 <78515942+vujnovicht9188@users.noreply.github.com>
2022-06-28 10:11:04 +1200

    shush

--- 01_base_component.py
+++ 01_base_component.py
@@ -1,3 +1,7 @@
+import re
+from urllib import response
+
+
 def yes_no(question):
 
     to_check = ['yes', 'no']
@@ -40,5 +44,93 @@ if want_help == "yes":
     print("insert instructions here")
     print()
     
+print()
 get_budget = num_check("What is your budget? $", "Please enter a number more than 0\n", float)
-print("your budget is ${}".format(get_budget))
+if get_budget == 8.99:
+    print()
+    print('Thank you for purchasing Pass Royale!')
+    print()
+
+print("Your budget is ${}".format(get_budget))
+
+# valid snacks holds list of all snacks
+# Each item in valid snacks is a list with'
+# valid options for each snack <full name, letter code (a - e)
+# , and possible abbreviations etc>
+valid_items = [
+    ["dark elixir", "d", "de", "darkelixir"],
+    ["elixir", "e"],
+    ["gold", "gl" ],
+    ["crowns", "crowns", "c",],
+    ["trophies", "trophy", "t"],
+    ["gems", "gem", "gm"],
+    ["xxx"]
+]
+
+# initialise variables
+item_ok = ""
+item = ""
+
+# loop three times to make testing quicker
+for item in range(0, 14):
+
+    item_valid = "valid"
+    while item_valid != "valid":
+
+    # ask user for desired item and put it in lowercase
+        print()    
+    desired_item = input("Item: ").lower()
+
+    if desired_item == "xxx":
+        break
+
+    for var_list in valid_items:
+
+        # if the item is in one of the lists, return the full name 
+        if desired_item in var_list:
+
+            # Get full name of item and put it 
+            # in title case so it looks nice when outputted
+            item = var_list[0].title()
+            item_ok = "yes"
+            break
+    
+        # if the chosen item is not valid, set item_ok to no
+        else:
+            item_ok = "no"
+        
+    # if the item is not OK - ask question again.'
+    
+    if item_ok == "yes":
+        print()
+        print("Item Choice:", item)
+        print()
+    else:
+        print("Invalid choice")
+        print()
+        continue
+    
+    unit_type = "invalid"
+    while unit_type != "valid":
+        get_unit = input("What measuremnt would you like to use? eg: (g, kg): ")
+        if get_unit == "g":
+            weight_type ="g"
+            unit_type = "valid"
+
+        elif get_unit == "kg":
+            weight_type = "kg"
+            unit_type = "valid"
+
+        else:
+            print("Error, please enter a valid unit type.")
+            print()
+            continue
+
+        
+
+    print()
+    get_weight = num_check("What is the weight of your item? ", "Error please enter valid number", float)
+    print()
+
+    print("Weight = {}".format(get_weight), weight_type)
+    print()
