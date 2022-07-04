1d791df5b1ea376e4baabd5d16f5135f8684e3ce
vujnovicht9188 <78515942+vujnovicht9188@users.noreply.github.com>
2022-07-05 11:00:58 +1200

    Add files via upload

---
+++ 03_yes_no_checker.py
@@ -0,0 +1,17 @@
+def yes_no(question):
+
+    to_check = ['yes', 'no']
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
+
